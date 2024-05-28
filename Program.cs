312278b4a4476505c6cc0fcbea7726aaef8fec9e
Ares-Judda <ares.judda@gmail.com>
2024-05-28 10:08:25 -0600

    backend.net

---
+++ Program.cs
@@ -0,0 +1,86 @@
+using Microsoft.EntityFrameworkCore;
+using backendnet.Data;
+using backendnet.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using backendnet.Services;
+using backendnet.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<JwtTokenService>();
+
+var connectionString = builder.Configuration.GetConnectionString("DataContext");
+builder.Services.AddDbContext<IdentityContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
+builder.Services.AddIdentity<CustomIdentityUser, IdentityRole>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+})
+.AddEntityFrameworkStores<IdentityContext>();
+
+builder.Services
+    .AddHttpContextAccessor()
+    .AddAuthorization()
+    .AddAuthentication(option => 
+{
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidAudience = builder.Configuration["JWT:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]!))
+    };
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3001", "http://localhost:8080")
+                .AllowAnyHeader()
+                .WithMethods("GET", "POST", "PUT", "DELETE");
+        });
+});
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseExceptionHandler("/error");
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseSlidingExpirationJwt();
+app.UseCors();
+app.MapControllers();
+app.Run();
